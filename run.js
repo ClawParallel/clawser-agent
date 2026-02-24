7dca67184bdca489590a62bfdcb0763f0e3377da
ClawParallel <clawparallel@gmail.com>
2026-02-24 23:33:30 +0700

    redeploy

--- run.js
+++ run.js
@@ -3,6 +3,8 @@ import { runAgent } from "./agent.js";
 
 console.log("🐾 CLAWSER DAILY AGENT ACTIVE");
 
+runAgent();
+
 cron.schedule("0 9 * * *", async () => {
   console.log("🐾 Daily hunt started...");
   await runAgent();
